68946f4f47acb8cdcea6da18d9f04a6f27b5061d
Sumit Prasad <prasadsumit1917@gmail.com>
2026-09-05 13:39:41 +0530

    initial commit

---
+++ src/main/java/com/prasadsumit/notebridge/NotebridgeApplication.java
@@ -0,0 +1,13 @@
+package com.prasadsumit.notebridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotebridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotebridgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prasadsumit/notebridge/NotebridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prasadsumit.notebridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotebridgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
